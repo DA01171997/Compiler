f29e41a0084c2ec3d5c517c150ac8096d7744827
= <duy.ado97@gmail.com>
2018-04-19 14:42:54 -0700

    Add main.cpp

--- main.cpp
+++ main.cpp
@@ -2,28 +2,37 @@
 #include <fstream>
 #include <string>
 #include <sstream>
-
+#include <algorithm>
 using namespace std;
 
+void parseComment();
+void parseSpace();
 int main(){
-	string line;
-	ofstream outFile;
-	ifstream inFile;
 	int tempHead;
 	int tempTail;
-	bool comment=false;
-	 try {
-        inFile.open("final.txt"); 
-		outFile.open("part1.txt", ofstream::out);
-        if (!inFile) {                                   
-            throw std::runtime_error("Can't open inFile");
-        }
-    }
-    catch (std::runtime_error &e) {                               
+	parseComment();
+	
+	return 0;
+}
+void parseComment(){
+		ofstream outFile;
+		ifstream inFile;
+		string line;
+		bool comment=false;
+		try {
+			inFile.open("final.txt"); 
+			outFile.open("part1.txt", ofstream::out);
+			if (!inFile) {                                   
+				throw std::runtime_error("Can't open inFile");
+			}
+		}
+		catch (std::runtime_error &e) {                               
         cout << e.what() << endl;
-    }
-    while (!inFile.eof()) {
+		}
+	    while (!inFile.eof()) {
 		getline(inFile, line);
+		line.erase(remove(line.begin(), line.end(), '\t'), line.end());
+		//line.erase(remove(line.begin(), line.end(), '\n'), line.end());
 		for(int i =0; i< line.length(); i++) {
 			if(line[i]=='(') {
 				if (line[i+1]=='*'){
@@ -40,10 +49,28 @@ int main(){
 				}
 			}
 		}
-		if(!comment){outFile<<endl;}
-		
+		if(!comment){outFile<<endl;}	
 	}
 	outFile.close();
 	inFile.close();
-	return 0;
+}
+void parseSpace() {
+		ofstream outFile2;
+		ifstream inFile2;
+		string line;
+		bool comment=false;
+		try {
+			inFile2.open("partl.txt"); 
+			outFile2.open("part2.txt", ofstream::out);
+			if (!inFile2) {                                   
+				throw std::runtime_error("Can't open inFile2");
+			}
+		}
+		catch (std::runtime_error &e) {                               
+        cout << e.what() << endl;
+		}
+	    while (!inFile2.eof()) {
+			
+		}
+	
 }
