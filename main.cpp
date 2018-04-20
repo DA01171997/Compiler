1989eeb55eab5b15cbce3735a0e2279f643c9598
jasonlieu <jasonlieuthefirst@gmail.com>
2018-04-19 18:13:00 -0700

    Used arrays because Duy

--- main.cpp
+++ main.cpp
@@ -15,24 +15,28 @@ int main(){
 	return 0;
 }
 void parseComment(){
-		ofstream outFile;
-		ifstream inFile;
-		string line;
-		bool comment=false;
-		try {
-			inFile.open("final.txt"); 
-			outFile.open("part1.txt", ofstream::out);
-			if (!inFile) {                                   
-				throw std::runtime_error("Can't open inFile");
-			}
+	ofstream outFile;
+	ifstream inFile;
+	string line;
+	bool comment=false;
+	bool readingToken = true;
+	bool emptyLine = true;
+	bool beginningOfLine;
+	try {
+		inFile.open("final.txt"); 
+		outFile.open("part1.txt", ofstream::out);
+		if (!inFile) {                                   
+			throw std::runtime_error("Can't open inFile");
 		}
-		catch (std::runtime_error &e) {                               
-        cout << e.what() << endl;
-		}
-	    while (!inFile.eof()) {
+	}
+	catch (std::runtime_error &e) {                               
+	cout << e.what() << endl;
+	}
+	while (!inFile.eof()) {
 		getline(inFile, line);
 		line.erase(remove(line.begin(), line.end(), '\t'), line.end());
 		//line.erase(remove(line.begin(), line.end(), '\n'), line.end());
+		beginningOfLine = true;
 		for(int i =0; i< line.length(); i++) {
 			if(line[i]=='(') {
 				if (line[i+1]=='*'){
@@ -40,7 +44,29 @@ void parseComment(){
 				}
 			}
 			if(!comment) {
-			outFile<<line[i];
+				if(line[i] != ' ' && line[i] != ',' && line[i] != ';' &&
+					line[i] != '='&&line[i] != '+'&&line[i] != '-'&&line[i] != '*'&&line[i] != ':' ){			//readingToken is true when line[i] != ' '
+					if(!readingToken && !beginningOfLine){														// 				  false when line[i] hits a ' '
+						outFile<<' ';
+					}
+					outFile<<line[i];
+					beginningOfLine = false;
+					readingToken = true;
+					emptyLine = false;
+				}
+				if(line[i] == ';'){
+					outFile<<';';
+				}
+				if(line[i] ==','){
+					outFile<<',';
+				}
+				if((line[i] == '='||line[i] == '+'||line[i] == '-'||line[i] == '*'||line[i] == ':' )){
+					outFile<<' '<<line[i];
+					if(line[i+1]!=' '){outFile<<' ';}
+				}
+				if(line[i] == ' ' && readingToken == true){
+					readingToken = false;
+				}
 			}
 			if(line[i]=='*') {
 				if (line[i+1]==')'){
@@ -49,7 +75,10 @@ void parseComment(){
 				}
 			}
 		}
-		if(!comment){outFile<<endl;}	
+		if(!comment && !emptyLine){
+			outFile<<endl;
+			emptyLine = true;
+		}	
 	}
 	outFile.close();
 	inFile.close();
