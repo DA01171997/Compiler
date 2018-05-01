3c1861f8b52a9303b92d4cc1fd666ea5655cb4cf
= <duy.ado97@gmail.com>
2018-05-01 15:13:48 -0700

    Update

--- test.sh
+++ test.sh
@@ -1,12 +1,5 @@
-echo "***Clean****"
-rm *.exe #part1.txt part2.txt
-echo "***Run make.***"
+echo "make"
 make
+echo "run"
+./lang < part2.txt
 
-echo "***Run main.exe.***"
-./main.exe
-echo "***Cat part1.exe***"
-cat part1.txt
-echo "***Cat Part2.exe***"
-cat part2.txt
-echo "***Build script has terminated.***"
